82dec515cfd5f2be1dfef43e68012bfcdadb5275
Rachel Gu <raegu14@gmail.com>
2017-09-18 00:48:43 -0400

    set up test with sphere and ramp, gravity and antigravity

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,15 +4,89 @@ using UnityEngine;
 
 public class Test : MonoBehaviour {
 
-    public GameObject cam;
+    [Header("Adding Force")]
+    public float constant;
+    public bool addForce;
+
+    [Header("Angular Drag")]
+    public float airDrag;
+    public float floorDrag;
+
+    [Header("Reset")]
+    public bool reset;
+    private Vector3 origin;
+
+    [Header("Gravity")]
+    public bool gravity;
+
+    private Rigidbody rb;
+    private bool colliding;
 
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity = new Vector3(0f, -5f, 0f);
+        origin = transform.position;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (addForce)
+        {
+            AddForce();
+        }
+
+        if (reset)
+        {
+            resetPosition();
+        }
+
+        if (gravity)
+        {
+            if (colliding)
+            {
+                rb.angularDrag = floorDrag;
+                Physics.gravity = new Vector3(0f, -5f, 0f);
+            }
+            else
+            {
+                rb.angularDrag = airDrag;
+                Physics.gravity = new Vector3(0f, -10f, 0f);
+            }
+        }
+        else
+        {
+            Physics.gravity = new Vector3();
+        }
+    }
+
+    void AddForce()
+    {
+        rb.AddForce(new Vector3(-1f, 0f) * constant);
+        addForce = false;
+    }
+
+    void resetPosition()
+    {
+        transform.position = origin;
+        rb.angularVelocity = new Vector3();
+        rb.velocity = new Vector3();
+        reset = false;
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        colliding = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        colliding = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        colliding = false;
+    }
 }
