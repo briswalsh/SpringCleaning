e610a1176a988471006e049eb5f04bdb5e8851a4
Rachel Gu <raegu14@gmail.com>
2017-09-20 22:39:12 -0400

    working on Vacuum Physics

--- Assets/Scripts/SpherePhysics.cs
+++ Assets/Scripts/SpherePhysics.cs
@@ -18,6 +18,7 @@ public class SpherePhysics : MonoBehaviour, IPhysics {
 
     [Header("Gravity")]
     public bool gravity;
+    public float gravityValue;
 
     private Rigidbody rb;
     private bool colliding;
@@ -25,7 +26,7 @@ public class SpherePhysics : MonoBehaviour, IPhysics {
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
-        Physics.gravity = new Vector3(0f, -5f, 0f);
+        Physics.gravity = new Vector3(0f, -10f, 0f);
         origin = transform.position;
 	}
 
@@ -34,11 +35,13 @@ public class SpherePhysics : MonoBehaviour, IPhysics {
         if (triggerForce)
         {
             AddForce();
+            triggerForce = false;
         }
 
         if (reset)
         {
             ResetPosition();
+            reset = false;
         }
 
         if (gravity)
@@ -46,7 +49,7 @@ public class SpherePhysics : MonoBehaviour, IPhysics {
             if (colliding)
             {
                 rb.angularDrag = floorDrag;
-                Physics.gravity = new Vector3(0f, -5f, 0f);
+                Physics.gravity = new Vector3(0f, -10f, 0f);
             }
             else
             {
@@ -56,20 +59,30 @@ public class SpherePhysics : MonoBehaviour, IPhysics {
         }
         else
         {
-            Physics.gravity = new Vector3();
+            if (colliding)
+            {
+                rb.angularDrag = floorDrag;
+                Physics.gravity = new Vector3();
+            }
+            else
+            {
+                rb.angularDrag = airDrag;
+                Physics.gravity = new Vector3();
+            }
         }
+        gravityValue = Physics.gravity.y;
     }
+
+    /* Helper Functions */
 
     private void AddForce()
     {
         rb.AddForce(new Vector3(-1f, 0f) * constant);
-        triggerForce = false;
     }
 
-    private void AddForce(int force)
+    private void AddForce(int force, Vector3 dir)
     {
-        rb.AddForce(new Vector3(-1f, 0f) * force);
-        triggerForce = false;
+        rb.AddForce(dir.normalized * force);
     }
 
     void ResetPosition()
@@ -77,9 +90,26 @@ public class SpherePhysics : MonoBehaviour, IPhysics {
         transform.position = origin;
         rb.angularVelocity = new Vector3();
         rb.velocity = new Vector3();
-        reset = false;
     }
 
+    /* Interface Implementation */
+
+    public void Hit(int force, Vector3 dir)
+    {
+        AddForce(force, dir);
+    }
+
+    public void Reset()
+    {
+        ResetPosition();
+    }
+
+    public void GravityControl(bool on)
+    {
+        gravity = on;
+    }
+
+    /* Collision Detectors */
     private void OnCollisionStay(Collision collision)
     {
         colliding = true;
@@ -93,15 +123,5 @@ public class SpherePhysics : MonoBehaviour, IPhysics {
     private void OnCollisionExit(Collision collision)
     {
         colliding = false;
-    }
-
-    public void Hit(int force)
-    {
-        AddForce(force);
-    }
-
-    public void Reset()
-    {
-        ResetPosition();
     }
 }
---
+++ Assets/Scripts/VacuumPhysics.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VacuumPhysics : MonoBehaviour {
+
+    [Header("Vacuum")]
+    public bool vacuumSwitch;
+    public float vacuumStr;
+    private bool vacuumOn;
+
+    public float t;
+
+    private GameObject movable;
+
+	// Use this for initialization
+	void Start () {
+        movable = GameObject.FindGameObjectWithTag("Movable");
+        t = vacuumStr;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (movable != null)
+        {
+            if(vacuumSwitch != vacuumOn)
+            {
+                VacuumControl(vacuumSwitch);
+            }
+            if (vacuumOn)
+            {
+                int len = movable.transform.childCount;
+                for (int i = 0; i < len; i++)
+                {
+                    Pull(movable.transform.GetChild(i));
+                }
+            }
+        }
+	} 
+
+    void Pull(Transform obj)
+    {
+        Vector3 newpos = Vector3.Lerp(obj.position, transform.position, t * (vacuumStr + 1));
+        obj.position = newpos;
+        t *= vacuumStr + 1;
+    }
+
+    void VacuumControl(bool on)
+    {
+        if(on && !vacuumOn)
+        {
+            t = vacuumStr;
+            vacuumOn = true;
+        }
+        else if (!on && vacuumOn)
+        {
+            t = 0f;
+            vacuumOn = false;
+        }
+    }
+}
