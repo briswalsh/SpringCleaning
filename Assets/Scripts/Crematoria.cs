c65ef301a730b817fc1e7b5f03dc4da89a94cff9
Brad Factor <brad.factor@gmail.com>
2017-09-28 01:30:01 -0400

    Updated crematoria script, still needs work. Adding sfx from Ryan.

--- Assets/Scripts/Crematoria.cs
+++ Assets/Scripts/Crematoria.cs
@@ -8,27 +8,33 @@ public class Crematoria : MonoBehaviour
     public GameObject fire;
     public GameObject youWillBeBaked; //User camera object
     public AudioSource thereWillBeCake; //The flame sound of your demise
+    private bool oneTime;
 
     // Use this for initialization
     void Start()
     {
         thereWillBeCake = GetComponent<AudioSource>();
-        Vector3 userPosn = youWillBeBaked.transform.position;
-
-        GameObject fireFront = Instantiate(fire);
-        GameObject fireLeft = Instantiate(fire);
-        GameObject fireRight = Instantiate(fire);
-        GameObject fireBack = Instantiate(fire);
-
-        fireFront.transform.position = userPosn + Vector3(2, 0, 0);
-        fireLeft.transform.position = userPosn + Vector3(0, 2, 0);
-        fireRight.transform.position = userPosn + Vector3(0, -2, 0);
-        fireBack.transform.position = userPosn + Vector3(-2, 0, 0);
+        oneTime = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(oneTime == false)
+        {
+            oneTime = true;
+            Vector3 userPosn = youWillBeBaked.transform.position;
+
+            GameObject fireFront = Instantiate(fire);
+            GameObject fireLeft = Instantiate(fire);
+            GameObject fireRight = Instantiate(fire);
+            GameObject fireBack = Instantiate(fire);
+
+            fireFront.transform.position = userPosn + Vector3.forward;
+            fireLeft.transform.position = userPosn + Vector3.left;
+            fireRight.transform.position = userPosn - Vector3.forward;
+            fireBack.transform.position = userPosn + Vector3.right;
+        }
 
     }
 }
