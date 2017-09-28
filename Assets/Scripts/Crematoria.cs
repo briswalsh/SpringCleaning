435afa5b2ceef1cc099e6c054381b577dab308b2
bfactor <bfactor@andrew.cmu.edu>
2017-09-28 01:00:23 -0400

    Started crematoria (game lose) script

---
+++ Assets/Scripts/Crematoria.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crematoria : MonoBehaviour
+{
+    [Header("Flame On")]
+    public GameObject fire;
+    public GameObject youWillBeBaked; //User camera object
+    public AudioSource thereWillBeCake; //The flame sound of your demise
+
+    // Use this for initialization
+    void Start()
+    {
+        thereWillBeCake = GetComponent<AudioSource>();
+        Vector3 userPosn = youWillBeBaked.transform.position;
+
+        GameObject fireFront = Instantiate(fire);
+        GameObject fireLeft = Instantiate(fire);
+        GameObject fireRight = Instantiate(fire);
+        GameObject fireBack = Instantiate(fire);
+
+        fireFront.transform.position = userPosn + Vector3(2, 0, 0);
+        fireLeft.transform.position = userPosn + Vector3(0, 2, 0);
+        fireRight.transform.position = userPosn + Vector3(0, -2, 0);
+        fireBack.transform.position = userPosn + Vector3(-2, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
