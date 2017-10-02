2c3a79005f00e72fb2c03cc24def0f3058b685ae
briswalsh <bswalsh101@gmail.com>
2017-10-01 23:54:19 -0400

    Haptics shell code.

---
+++ Assets/Scripts/Player/HapticController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Controller { Left, Right }
+
+public class HapticController : MonoBehaviour
+{
+    private List<OVRHapticsClip> clips = new List<OVRHapticsClip>();
+
+    public void Add(AudioClip clip, Controller cont)
+    {
+        if (cont == 0)
+        {
+            clips.Add(new OVRHapticsClip(clip, 0));
+        }
+        else
+        {
+            clips.Add(new OVRHapticsClip(clip, 1));
+        }
+    }
+}
